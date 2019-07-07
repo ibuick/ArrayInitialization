7a6233b58922898eeb0274e38f8ed961ea127cb1
Buick Wong <ibuick.com@gmail.com>
2019-07-07 23:31:08 +0800

    Update Main.swift
    because Double.random caused heavy os_unfair_lock/unlock, try to use Intel RDRAND instruction directly.

--- Main.swift
+++ Main.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 let COUNT = 1500000
-let BASE  = 150000
+let BASE  = 15000
 let BIAS  = COUNT/BASE
 
 func runTime(for name: String, _ closure: () -> ()) {
@@ -21,25 +21,6 @@ func runTime(for name: String, _ closure: () -> ()) {
 var firstArray = Array<Double>(repeating: 0.0, count: COUNT)
 var secondArray = Array<Double>(repeating: 0.0, count: COUNT)
 
-runTime(for: "Sequential Create Source Array") {
-
-    firstArray.withUnsafeMutableBufferPointer { arrayBuffer in
-        for i in 0..<COUNT {
-            arrayBuffer[i] = Double.random(in: 0..<10)
-        }
-    }
-
-    secondArray.withUnsafeMutableBufferPointer { arrayBuffer in
-        for i in 0..<COUNT {
-            arrayBuffer[i] = Double.random(in: 0..<10)
-        }
-    }
-
-}
-
-firstArray = Array<Double>(repeating: 0.0, count: COUNT)
-secondArray = Array<Double>(repeating: 0.0, count: COUNT)
-
 runTime(for: "Parallel Create Source Array") {
 
     DispatchQueue.concurrentPerform(iterations: 1, execute: { idx in
@@ -47,8 +28,10 @@ runTime(for: "Parallel Create Source Array") {
         firstArray.withUnsafeMutableBufferPointer { arrayBuffer in
             DispatchQueue.concurrentPerform(iterations: (BIAS), execute: { idx in
                 let base = idx * BASE
+                var output: CDouble = 0.0
                 for j in base..<(base + BASE) {
-                    arrayBuffer[j] = Double.random(in: 0..<10)
+                    rdrand64_step(&output)
+                    arrayBuffer[j] = output
                 }
             })
         }
@@ -56,8 +39,10 @@ runTime(for: "Parallel Create Source Array") {
         secondArray.withUnsafeMutableBufferPointer { arrayBuffer in
             DispatchQueue.concurrentPerform(iterations: (BIAS), execute: { idx in
                 let base = idx * BASE
+                var output: CDouble = 0.0
                 for j in base..<(base + BASE) {
-                    arrayBuffer[j] = Double.random(in: 0..<10)
+                    rdrand64_step(&output)
+                    arrayBuffer[j] = output
                 }
             })
         }
@@ -65,3 +50,33 @@ runTime(for: "Parallel Create Source Array") {
     })
 
 }
+
+print(firstArray[0..<5])
+print(secondArray[0..<5])
+
+firstArray = Array<Double>(repeating: 0.0, count: COUNT)
+secondArray = Array<Double>(repeating: 0.0, count: COUNT)
+
+runTime(for: "Sequential Create Source Array") {
+
+    firstArray.withUnsafeMutableBufferPointer { arrayBuffer in
+        var output: CDouble = 0.0
+        for i in 0..<COUNT {
+            rdrand64_step(&output)
+            arrayBuffer[i] = output
+        }
+    }
+
+    secondArray.withUnsafeMutableBufferPointer { arrayBuffer in
+        var output: CDouble = 0.0
+        for i in 0..<COUNT {
+            rdrand64_step(&output)
+            arrayBuffer[i] = output
+        }
+    }
+
+}
+
+
+
+
