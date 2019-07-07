1b2f3c724ef27d44097fc81546ef3a55279548bc
Buick Wong <ibuick.com@gmail.com>
2019-07-07 23:31:46 +0800

    Create rand.h
    rand.h

---
+++ rand.h
@@ -0,0 +1,14 @@
+//
+//  rand.h
+//  tex
+//
+//  Created by Buick Wong on 7/7/19.
+//  Copyright © 2019 Buick Wong. All rights reserved.
+//
+
+#ifndef rand_h
+#define rand_h
+
+#include <stdio.h>
+
+#endif /* rand_h */
